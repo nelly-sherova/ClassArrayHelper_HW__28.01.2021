c4490468eb8bb51f27d69a01f63f780f37ac23cf
unknown <nellysherova@mail.ru>
2021-01-29 20:31:57 +0300

    Добавила метод Push

--- ArrayHelper/Program.cs
+++ ArrayHelper/Program.cs
@@ -2,11 +2,50 @@
 
 namespace ArrayHelper
 {
+    public static class Array_Helper
+    {
+        public static string Pop(ref string[] arr)
+        {
+            string el = arr[arr.Length - 1];
+            Array.Resize(ref arr, arr.Length - 1);
+            return el;
+        }
+        public static int Pop(ref int[] arr)
+        {
+            int el = arr[arr.Length - 1];
+            Array.Resize(ref arr, arr.Length - 1);
+            return el;
+        }
+        public static double Pop(ref double[] arr)
+        {
+            double el = arr[arr.Length - 1];;
+            Array.Resize(ref arr, arr.Length - 1);
+            return el;
+        }
+        public static decimal Pop(ref decimal[] arr)
+        {
+            decimal el = arr[arr.Length - 1];
+            Array.Resize(ref arr, arr.Length - 1);
+            return el;
+        }
+        public static float Pop(ref float[] arr)
+        {
+            float el = arr[arr.Length - 1];
+            Array.Resize(ref arr, arr.Length - 1);
+            return el;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] arr1 = {1, 2, 3, 4, 5, 6, 7};
+            Console.WriteLine(Array_Helper.Pop(ref arr1));
+            foreach (var item in arr1)
+            {   
+                Console.Write(item);
+            }
+            Console.WriteLine(arr1.Length);
         }
     }
 }
