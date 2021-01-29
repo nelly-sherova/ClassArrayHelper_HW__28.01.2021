e0ae7586d838bee485f4d5f51498e65fe4fcc980
unknown <nellysherova@mail.ru>
2021-01-29 21:18:03 +0300

    Добавила метод Unshift с его перегрузками

--- ArrayHelper/Program.cs
+++ ArrayHelper/Program.cs
@@ -104,14 +104,53 @@ namespace ArrayHelper
             Array.Resize(ref arr, arr.Length - 1);
             return el;
         }
-
+        public static int UnShift(ref string[] arr, string el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            for (int i = arr.Length-1; i >= 1; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = el;
+            return arr.Length;
+        }
+        public static int UnShift(ref int[] arr, int el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            for (int i = arr.Length-1; i >= 1; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = el;
+            return arr.Length;
+        }
+        public static int UnShift(ref double[] arr, double el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            for (int i = arr.Length-1; i >= 1; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = el;
+            return arr.Length;
+        }
+        public static int UnShift(ref decimal[] arr, decimal el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            for (int i = arr.Length-1; i >= 1; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = el;
+            return arr.Length;
+        }
+        public static int UnShift(ref float[] arr, float el)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            for (int i = arr.Length-1; i >= 1; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = el;
+            return arr.Length;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             string[] arr1 = {"1", "2"," 3", "4", "5", "6", "7"};
-            Console.WriteLine(Array_Helper.Shift(ref arr1));
+            Console.WriteLine(Array_Helper.UnShift(ref arr1, "kljh"));
 
             foreach (var item in arr1)
             {   
